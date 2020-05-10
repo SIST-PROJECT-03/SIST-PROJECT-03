a1636280dd21588e318aef7db6863d6b3e58339d
HakMin <kimys260@naver.com>
2020-05-10 14:06:58 +0900

    Cast Data Insert

---
+++ MovieCastData1/src/com/sist/data/MainClass.java
@@ -0,0 +1,13 @@
+package com.sist.data;
+import java.util.*;
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CastManager cm=new CastManager();
+		try{
+			cm.castAllData();
+		}catch (Exception ex) {}
+	}
+
+}
