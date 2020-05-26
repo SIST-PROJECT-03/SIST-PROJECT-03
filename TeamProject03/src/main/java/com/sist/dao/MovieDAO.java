78f10c1640924e09a4cd3be935f359420db0bd48
sbcho0630 <sbcho0630@gmail.com>
2020-05-26 09:10:42 +0900

    디테일 예고편보기 추가

--- TeamProject03/src/main/java/com/sist/dao/MovieDAO.java
+++ TeamProject03/src/main/java/com/sist/dao/MovieDAO.java
@@ -1,10 +1,14 @@
 package com.sist.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.sist.mapper.MovieMapper;
 import com.sist.vo.MovieDetailVO;
+import com.sist.vo.MoviePicturesVO;
+import com.sist.vo.WatchingTrendVO;
 
 @Repository
 public class MovieDAO {
@@ -14,4 +18,14 @@ public class MovieDAO {
 	   {
 		   return mapper.getMovieDetailData(movie_id);
 	   }
+	public WatchingTrendVO getWatchingTrend(int movie_id){
+		return mapper.getWatchingTrend(movie_id);
+	}
+	public List<String> getMoviePictures(int movie_id){
+		return mapper.getMoviePictures(movie_id);
+	}
+	public List<String> getMovieUrl(int movie_id){
+		
+		return mapper.getMovieUrl(movie_id);
+	}
 }
--- TeamProject03/src/main/java/com/sist/mapper/MovieMapper.java
+++ TeamProject03/src/main/java/com/sist/mapper/MovieMapper.java
@@ -1,12 +1,24 @@
 package com.sist.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Select;
 
 import com.sist.vo.MovieDetailVO;
+import com.sist.vo.MoviePicturesVO;
+import com.sist.vo.WatchingTrendVO;
 
 public interface MovieMapper {
 	@Select("SELECT * FROM naver_re_movies WHERE movie_id=#{movie_id}")
 	public MovieDetailVO getMovieDetailData(int movie_id);
 	
+	@Select("SELECT * FROM watching_trend WHERE movie_id=#{movie_id}")
+	public WatchingTrendVO getWatchingTrend(int movie_id);
+	
+	@Select("SELECT img FROM movie_re_pictures WHERE movie_id=#{movie_id}")
+	public List<String> getMoviePictures(int movie_id);
+	
+	@Select("SELECT url FROM movie_re_pictures WHERE movie_id=#{movie_id} AND url IS NOT NULL")   
+	public List<String> getMovieUrl(int movie_id);
 	
 }
